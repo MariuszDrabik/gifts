77694fdd157dd5e0c6171dc5f22d27514aa73c25
Mariusz dell <drabik.mariusz@gmail.com>
2023-01-25 19:56:58 +0100

    record

--- records/gift.record.js
+++ records/gift.record.js
@@ -1,6 +1,30 @@
 import { conn } from "../utils/db.js"
+import { ValidationError } from "../utils/errors.js"
 
 class GiftRecord {
+    constructor(obj) {
+        if (obj.name.length() < 3 || obj.name.length() > 55) {
+            throw new ValidationError(
+                'Nazwa zabawki musi mieć pomiędzy 3 a 55 znaków'
+                )
+            }
+        if (obj.counter < 1 || obj.counter > 99999 || isNaN(obj.counter)) {
+            throw new ValidationError(
+                'To musi być liczba pomiędzy 1 a 99999'
+            )
+        }
+        this.id = obj.id;
+        this.name = obj.name;
+        this.count = obj.count
+    }
+    async saveGift() {
+        if(!this.id) {
+            this.id = uuid();
+            conn.ex
+        }
+    }
+
+
     static async listAll() {
           const [rows] = await conn.query("SELECT * FROM `Gifts`")
         return rows
